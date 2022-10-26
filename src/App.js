d127b375d8015bfa7c6b22124682462d30a7a55a
Luke Lai <lukelai.dev@gmail.com>
2022-10-26 11:37:02 +0100

    fetch shop data to shop page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/routes/navigation/navigation.component";
 import Home from "./components/routes/home/home.compoenent";
 import Authentication from "./components/routes/authentication/authentication.component";
+import Shop from "./components/routes/shop/shop.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="/shop" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
         <Route path="/auth" element={<Authentication />} />
       </Route>
     </Routes>
